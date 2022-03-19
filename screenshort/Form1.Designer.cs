30b0b49544a0eab6a0e4770e61c1bd46dc0677aa
kds-zahid <90888332+kds-zahid@users.noreply.github.com>
2022-03-19 08:41:14 -0700

    upload
    upload

---
+++ screenshort/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace screenshort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::screenshort.Properties.Resources.screenshot_alt_macos_bigsur_icon_189765;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(143, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 69);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.capture_keydown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 69);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 92);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(256, 131);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(256, 131);
+            this.Name = "Form1";
+            this.Text = "zahid_949551";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keydown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.keypress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ screenshort/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace screenshort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp;
+        Graphics gr;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CaptureMyScreen();
+        }
+        private void CaptureMyScreen()
+        {
+
+            try
+            {
+                Bitmap captureBitmap = bmp;
+                
+                Rectangle captureRectangle = Screen.AllScreens[0].Bounds;
+                Graphics captureGraphics = Graphics.FromImage(captureBitmap);
+                captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+                captureBitmap.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyPictures).ToString() + @"\capture"+ GenerateFileName("_zahid_949551_")+ ".jpg", ImageFormat.Jpeg);
+                MessageBox.Show("Screen Captured");
+
+
+                //MessageBox.Show();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        public string GenerateFileName(string context)
+        {
+            return context + "_" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + "_" ;
+            //+ Guid.NewGuid().ToString("N")
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(Screen.PrimaryScreen.WorkingArea.Width,
+                             Screen.PrimaryScreen.WorkingArea.Height);
+            gr = Graphics.FromImage(bmp);
+            gr.CopyFromScreen(0, 0, 0, 0, new Size(bmp.Width, bmp.Height));
+            pictureBox1.Image = bmp;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+        private void keypress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void keydown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void capture_keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.PrintScreen)
+            {
+                CaptureMyScreen();
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                CaptureMyScreen();
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
